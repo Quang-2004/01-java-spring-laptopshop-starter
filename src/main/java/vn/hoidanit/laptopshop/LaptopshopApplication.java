4b3460a90e635177c2c0cd872120d40d4b100d15
Quang-2004 <nguyen.v.quangtp@gmail.com>
2024-10-20 11:23:08 +0700

    new

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,19 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+		
+		// container
+		ApplicationContext quang = SpringApplication.run(LaptopshopApplication.class, args);
+		for(String s: quang.getBeanDefinitionNames()){
+			System.out.println(s);
+		}
 	}
 
 }
+
---
+++ src/main/java/vn/hoidanit/services/MyServices.java
@@ -0,0 +1,8 @@
+package vn.hoidanit.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyServices {
+    
+}
