3d4cddba2e6ecc755792ccc4b979880050f994c4
Quang-2004 <nguyen.v.quangtp@gmail.com>
2024-10-21 20:18:55 +0700

    new

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-    return "Hello World with Nguyen Van Quang!";
+    return "Hello World hihiii!";
     }
 }
--- src/main/java/vn/hoidanit/laptopshop/services/MyServices.java
+++ src/main/java/vn/hoidanit/laptopshop/services/MyServices.java
@@ -1,4 +1,4 @@
-package vn.hoidanit.services;
+package vn.hoidanit.laptopshop.services;
 
 import org.springframework.stereotype.Service;
 
