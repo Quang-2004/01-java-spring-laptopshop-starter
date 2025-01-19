b15d673bacb9e31b56e373f89c2dc77e179def82
Quang-2004 <nguyen.v.quangtp@gmail.com>
2025-01-19 12:22:29 +0700

    video 119

--- src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import jakarta.servlet.DispatcherType;
 import vn.hoidanit.laptopshop.service.CustomUserDetailsService;
@@ -28,6 +29,7 @@ public class SecurityConfiguration {
         return new CustomUserDetailsService(userService);
     }
 
+    // noi cho thằng spring biết ta đang dùng bcryPasswordEncoder 
     @Bean
     public DaoAuthenticationProvider authProvider(
             PasswordEncoder passwordEncoder,
@@ -35,10 +37,11 @@ public class SecurityConfiguration {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder);
-        // authProvider.setHideUserNotFoundExceptions(false);
+        // authProvider.setHideUserNotFoundExceptions(false); // hien thi thog bao user not found
         return authProvider;
     }
 
+    // custom login page and ...
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -48,13 +51,21 @@ public class SecurityConfiguration {
                 // key INCLUDE cho phép 1 trang lấy dc thông tin ở các nơi khác
                 // vd: home lấy list product
                 .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE) .permitAll()
-                .requestMatchers("/", "/login","/register" , "/client/**", "/css/**", "/js/**", "/images/**").permitAll()
+                .requestMatchers("/", "/login","/register" , "/client/**", "/css/**", "/js/**", "/images/**", "/product/**").permitAll()
+                .requestMatchers("/admin/**").hasRole("ADMIN") 
+                .requestMatchers("/client/**").hasRole("USER") 
                 .anyRequest().authenticated())
 
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
                         .failureUrl("/login?error")
+                        .successHandler(customSuccessHandler())
                         .permitAll());
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler customSuccessHandler() {
+        return new CustomSuccessHandler();
     }
 }
