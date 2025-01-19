be1b7ab31c8882eff3a6b7d58683f202ec88cf58
Quang-2004 <nguyen.v.quangtp@gmail.com>
2025-01-19 17:58:16 +0700

    video 122

--- src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 
 import jakarta.servlet.DispatcherType;
 import vn.hoidanit.laptopshop.service.CustomUserDetailsService;
@@ -29,7 +31,7 @@ public class SecurityConfiguration {
         return new CustomUserDetailsService(userService);
     }
 
-    // noi cho thằng spring biết ta đang dùng bcryPasswordEncoder 
+    // noi cho thằng spring biết ta đang dùng bcryPasswordEncoder
     @Bean
     public DaoAuthenticationProvider authProvider(
             PasswordEncoder passwordEncoder,
@@ -37,24 +39,46 @@ public class SecurityConfiguration {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder);
-        // authProvider.setHideUserNotFoundExceptions(false); // hien thi thog bao user not found
+        // authProvider.setHideUserNotFoundExceptions(false); // hien thi thog bao user
+        // not found
         return authProvider;
+    }
+
+    @Bean
+    public SpringSessionRememberMeServices rememberMeServices() {
+        SpringSessionRememberMeServices rememberMeServices = new SpringSessionRememberMeServices();
+
+        // optionally customize
+        rememberMeServices.setAlwaysRemember(true);
+        return rememberMeServices;
     }
 
     // custom login page and ...
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                
+
                 .authorizeHttpRequests(authorize -> authorize
-                // key FORWARD cho phép rederiect tới login
-                // key INCLUDE cho phép 1 trang lấy dc thông tin ở các nơi khác
-                // vd: home lấy list product
-                .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE) .permitAll()
-                .requestMatchers("/", "/login","/register" , "/client/**", "/css/**", "/js/**", "/images/**", "/product/**").permitAll()
-                .requestMatchers("/admin/**").hasRole("ADMIN") 
-                .requestMatchers("/client/**").hasRole("USER") 
-                .anyRequest().authenticated())
+                        // key FORWARD cho phép rederiect tới login
+                        // key INCLUDE cho phép 1 trang lấy dc thông tin ở các nơi khác
+                        // vd: home lấy list product
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE).permitAll()
+                        .requestMatchers("/", "/login", "/register", "/client/**", "/css/**", "/js/**", "/images/**",
+                                "/product/**")
+                        .permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/client/**").hasRole("USER")
+                        .anyRequest().authenticated())
+
+                .rememberMe(r -> r.rememberMeServices(rememberMeServices()))
+
+                .sessionManagement((sessionManagement) -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+
+                        .invalidSessionUrl("/logout?expired")
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(false))
+                // .logout(logout->logout.deleteCookies("JSESSIONID").invalidateHttpSession(true))
 
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
@@ -62,7 +86,7 @@ public class SecurityConfiguration {
                         .successHandler(customSuccessHandler())
                         .permitAll())
                 .exceptionHandling(ex -> ex.accessDeniedPage("/access-deny"));
-                
+
         return http.build();
     }
 
